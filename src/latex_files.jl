fe3ac33ec828f60bb4833f63c98a106b3b258b0b
Fabricio Oliveira <fabricio.oliveira@aalto.fi>
2021-09-16 10:34:05 +0300

    Fix folder access and pdflatex compiling

--- src/latex_files.jl
+++ src/latex_files.jl
@@ -1,16 +1,17 @@
 
 function latex_files(dir::String, output = "./pdfs")  
-    for lecture_folder in readdir(dir)
-        if isdir(lecture_folder) && occursin("Lecture_", lecture_folder)
+    for lecture_folder in readdir(dir)        
+        lecture_path = joinpath(dir,lecture_folder)
+        if isdir(lecture_path) && occursin("Lecture_", lecture_folder)
             println("Compiling $lecture_folder...")
             handout = lecture_folder * "-slide_handout.tex"
             annotated = lecture_folder * "-slide_annotated.tex"
             note = lecture_folder * "-notes.tex" 
-            cd(lecture_folder)
+            cd(lecture_path)
             latex_file(output, handout)
             latex_file(output, annotated)
             latex_file(output, note)
-            cd("..")    
+            cd("../..")    
         end 
     end
 end    
@@ -19,6 +20,7 @@ function latex_file(output::String, latex_file::String)
     !isdir(output) && mkdir(output)
     
     command = `pdflatex -file-line-error -interaction=batchmode -synctex=1 -output-directory=$(output) $(latex_file)`
+    run(command)
     run(command)
 
     # Removes intermediate files from latex compilation
