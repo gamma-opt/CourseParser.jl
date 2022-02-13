5858fcd8fe382dba311481f4da8f90a483cf21e7
solliolli <olli.herrala@aalto.fi>
2022-02-13 13:51:16 +0200

    Add possibility to replace a piece of text with another in the skeleton. Also indentation works now.

--- src/weave_exercises.jl
+++ src/weave_exercises.jl
@@ -10,7 +10,9 @@ function _generate_skeleton(file_name::AbstractString)
 
     open(skeleton_file, "w+") do f
         for line in lines
-            startswith(strip(line), "#%") ? write(f, "\n") : write(f, line)
+            if !startswith(strip(line), "#%")
+                write(f, line)
+            end
         end
     end
 
@@ -25,28 +27,52 @@ Converts a file `exercise_complete.ipynb` into `exercise.jl`. Use this to genera
 Assumes that inside each code cell, the parts that should be removed from the skeleton are
 separated by lines corresponding to the parameter _keyword_.
 ```
-function _generate_script_from_solution(file_name::AbstractString; keyword="#%")
+function _generate_script_from_solution(file_name::AbstractString; if_keyword="#%", else_keyword="#%%")
     doc = Weave.WeaveDoc(file_name)
     for chunk in filter(chunk -> typeof(chunk) == Weave.CodeChunk, doc.chunks)
-        flag = false
+        indentlevel = 0
+        if_flag = false
+        else_flag = false
         new_content = ""
         for line in split(chunk.content,"\n")
-            if flag
-                if strip(line) == keyword
-                    flag = false
+            if if_flag
+                if strip(line) == if_keyword
+                    if !else_flag
+                        new_content *= ("    "^indentlevel)*"# TODO: add your code here\n"
+                    end
+                    if_flag = false
+                    else_flag = false
+                elseif strip(line) == else_keyword
+                    else_flag = true
                 else
-                    new_content *= "#% " * line * "\n"
+                    if else_flag
+                        new_line = strip(chop(strip(line), head=1, tail=0))
+                        new_content *= ("    "^indentlevel)*new_line * "\n"
+                        if startswith(new_line, r"function |if |for |while")
+                            indentlevel += 1
+                        elseif strip(new_line) == "end"
+                            indentlevel -= 1
+                        end
+                    else
+                        new_content *= "#% " * line * "\n"
+                    end
                 end
             else
-                if strip(line) == keyword
-                    flag = true
-                    new_content *= "# TODO: add your code here\n"
+                if strip(line) == if_keyword
+                    if_flag = true
+                elseif strip(line) == else_keyword
+                    @warn("Keyword " * else_keyword * " outside " * if_keyword * " in code cell number $(chunk.number), it will be ignored!")
                 else
                     new_content *= line * "\n"
+                    if startswith(strip(line), r"function |if |for |while")
+                        indentlevel += 1
+                    elseif strip(line) == "end"
+                        indentlevel -= 1
+                    end
                 end
             end
         end
-        if flag
+        if if_flag
             @warn("Unmatched " * keyword * " in code cell number $(chunk.number), cell is passed through as is!")
         else
             chunk.content = new_content
