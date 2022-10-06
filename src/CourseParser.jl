bc94a4b08605b04f3a9adfad2c2ffb589a38682c
Olli Herrala <43684983+solliolli@users.noreply.github.com>
2022-10-06 10:01:20 +0300

    Remove old functions
    Most of the functions here don't exist anymore

--- src/CourseParser.jl
+++ src/CourseParser.jl
@@ -3,11 +3,7 @@ module CourseParser
 include("weave_exercises.jl")
 include("latex_files.jl")
 
-export generate_solution,
-       generate_skeleton,
-       generate_script_from_solution,
-       generate_processed_script_from_solution,
-       generate_notebooks
+export generate_skeleton
 
 # export latex_files,
 #        latex_file        
